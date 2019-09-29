52c0f508626c5560bbebf3fc610f41afb498836f
Sarneet Longia <sarneet.longia@gmail.com>
2019-09-29 02:04:24 -0400

    Schema Development Completed

---
+++ models/instructions.js
@@ -0,0 +1,26 @@
+module.exports = function(sequelize, DataTypes) {
+  var Instructions = sequelize.define("Instructions", {
+    Number: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    Step: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 0
+    }
+  });
+  Instructions.associate = function(models) {
+    models.Instructions.belongsTo(models.Recipes, {
+      onDelete: "CASCADE",
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Instructions;
+};
